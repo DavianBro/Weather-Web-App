562aff185ec0998b853f7bdd9ae95df7aec4b4f3
DavianBro <74699056+DavianBro@users.noreply.github.com>
2021-08-06 01:46:33 -0400

    Current Weather Files 1st Commit

---
+++ WeatherWebApp /src/main/java/com/example/WeatherWebApp/CurrentWeather.java
@@ -0,0 +1,19 @@
+package com.example.WeatherWebApp;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class CurrentWeather implements Serializable {
+
+    private String description;
+    private BigDecimal temperature;
+    private BigDecimal feelsLike;
+    private BigDecimal windSpeed;
+
+    // Getters
+    // Setters
+    // Methods 
+
+
+
+}
